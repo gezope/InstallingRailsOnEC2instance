bde4b5f91812f8cbe3b0968a535a046667996186
Zoltan Gero <gezope@gmail.com>
2011-05-25 03:27:47 +0200

    step 7 added

---
+++ step7onEC2.rb
@@ -0,0 +1,5 @@
+# Step 7: Install Rails and other gems
+
+$ gem install rails haml compass aws-ses aws-s3 authlogic cucumber datamapper json mongo mongoid oauth authlogic-oauth padrino omniauth pdf-reader pg rack rack-cache sinatra sqlite3 thin
+
+# or choose whatever you need from http://ruby-toolbox.com/
