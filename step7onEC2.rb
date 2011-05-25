7d6a8af2e8d5ca3ec30b7a1e1041aa085826eb49
Zoltan Gero <gezope@gmail.com>
2011-05-25 03:29:57 +0200

    check if all works fine

--- step7onEC2.rb
+++ step7onEC2.rb
@@ -2,4 +2,11 @@
 
 $ gem install rails haml compass aws-ses aws-s3 authlogic cucumber datamapper json mongo mongoid oauth authlogic-oauth padrino omniauth pdf-reader pg rack rack-cache sinatra sqlite3 thin
 
+# Important: check if everything works fine
+$ rvm -v
+$ rvm list
+$ ruby -v
+$ rails -v
+$ gem list
+
 # or choose whatever you need from http://ruby-toolbox.com/
